32cb6000f29bc74d8eef99b51c0c0d68ee11fd03
Christopher Larson <chjlarson@gmail.com>
2018-10-24 13:11:55 -0500

    C# Practice

---
+++ Constructors/Constructors/Customers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Orders;
+
+        // Defualt Constructor
+        public Customer()
+        {
+            Orders = new List<Order>;
+        }
+
+        // Overloaded Constructors
+        // "this" used to pass the execution to another constructors in this class
+        public Customer(int id)
+            :this()
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name)
+            :this(id)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(12, "John");
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+
+            var order = new Order();
+            customer.Orders.Add(order)
+        }
+    }
+}    
